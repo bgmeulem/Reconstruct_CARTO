62049fb6fd22e25a5cdbe22ebb9646c565feb8b4
bgmeulem <38312921+bgmeulem@users.noreply.github.com>
2021-09-08 17:16:41 +0200

    Cleaned up carto2csv.py

--- carto2csv.py
+++ carto2csv.py
@@ -36,25 +36,6 @@ def cartoToCsv(inmesh, meshdir="", verbose=True):
                 if section_ind > 1:  # skip GeneralAttributes
                     of = open(meshdir + section + ".csv", "w+", encoding='utf-8')
 
-                # New tqdm loops per section
-                # if section_ind > 1:
-                #     if section_ind > 2:
-                #         time.sleep(1e-6)
-                #         loop.close()  # close previous loop
-                #     if section_ind == 3:
-                #         time.sleep(1e-6)
-                #         loop = tqdm(desc='        ' + section, total=n_triangles, position=0, leave=True)
-                #     else:
-                #         time.sleep(1e-6)
-                #         loop = tqdm(desc='        ' + section, total=n_vertices, position=0, leave=True)
-
-            # extract n_vertices and n_triangles to use for tqdm loops
-            # elif section_ind == 1:
-            #     if current_line == section_line + 3:  # n_verticess
-            #         n_vertices = int(line.split("=")[1])
-            #     elif current_line == section_line + 4:
-            #         n_triangles = int(line.split('=')[1])
-
             elif section_ind > 1:  # useful data
                 if section_ind == 4:
                     header_line = section_line + 2  # VerticesColorSection has extra line
@@ -67,23 +48,18 @@ def cartoToCsv(inmesh, meshdir="", verbose=True):
                     for name in column_names:
                         of.write("," + name)
                     of.write("\n")
-                    # time.sleep(1e-8)
 
                 elif len(line) > 1 and line[0] != ";":  # actual data, not empty line or column names
-                    # time.sleep(1e-8)
-                    # loop.update(1)
                     ind, data = line.split("=")  # line has shape "ind = x  y  z  nx  ny  nz  groupID
                     of.write(str(ind))
                     for el in data.split():  # ignore ind
                         of.write("," + str(el))
                     of.write("\n")
-        # time.sleep(1e-8)
-        # loop.close()
-        # time.sleep(1e-8)
+
         of.close()
 
 
 if __name__ == '__main__':
-    infile = '1-AT1230CL.mesh'
+    infile = '1-AT1230CL.mesh'  # test
     cartoToCsv(infile)
 
